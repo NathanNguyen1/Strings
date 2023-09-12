948250450630349b2cd314b05391e35b5f9de720
Nathan Nguyen <nathan.nguyen@mrjaffesclass.com>
2023-09-12 11:02:11 -0700

    Commit message

---
+++ Strings.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Strings
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
+        i = 10;
+        f +=i;
+        flag = !flag;
+        i++;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
+        String string1 = "I am a string";
+        String string2 = string1;
+        String string3 = "I am also a string";
+        boolean stringsEqual = string1.equals(string2);
+    }
+}
